e06632ffd4a2b46f299ae73ecaa452087b026d81
Lordpedal <39012148+Lordpedal@users.noreply.github.com>
2022-05-22 09:24:33 +0200

    Actualización dom 22 may 2022 09:24:33 CEST

--- lordpedal.sh
+++ lordpedal.sh
@@ -3,15 +3,16 @@
 # http://lordpedal.github.io
 # Another fine release by Lordpedal
 #
-function actualiza_lista {
-        wget $M3U_URL -O ./media/Lordpedal.m3u
-        return 0
-}
-# Bucle actualización
-while true
-do
-        sleep 1800 && actualiza_lista
-done &
-# Inicia Script
-actualiza_lista
+## Check
+#function actualiza_lista {
+#        wget $M3U_URL -O ./media/Lordpedal.m3u
+#        return 0
+#}
+## Bucle actualización
+#while true
+#do
+#        sleep 1800 && actualiza_lista
+#done &
+## Inicia Script
+#actualiza_lista
 ./xupnpd
